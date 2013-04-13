007773da14a1ab35b74fdcc5f1bf74201792e241
Rob Manning <Jman_1106@gcsu202044.gcsu.edu>
2013-04-13 15:19:07 -0400

    Fixed mispelling

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>The PupFetchet</title>
+<title>The PupFetcher</title>
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link rel="Stylesheet" type="text/css" href="css/smoothDivScroll.css" />
 </head>
