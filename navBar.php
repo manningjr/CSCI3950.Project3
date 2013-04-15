c40fa2071d031a1608d1e5794ba19c18268e3ac9
Nick Morelli <morelli3135@Nick-Morellis-MacBook-Pro.local>
2013-04-15 16:13:28 -0400

    I made the code look better

--- navBar.php
+++ navBar.php
@@ -2,7 +2,10 @@
 <div class="navigation"><li id="Home">Home</li><li id="Puppies">Puppies</li><li id="Info">Info</li></div>
 
 <div class="nav-letters">
-<li id="Home"><a href="index.php"><img src="img/paw.png" name="paw" width="15" height="15" /> Home</a></li><li id="Puppies"><a href="puppies.html"><img src="img/paw.png" name="paw" width="15" height="15" /> Puppies</a></li><li id="Info"><a href="info.html"><img src="img/paw.png" name="paw" width="15" height="15"/> Info</a> </li></div>
+	<li id="Home"><a href="index.php"><img src="img/paw.png" name="paw" width="15" height="15" /> Home</a></li>
+	<li id="Puppies"><a href="puppies.html"><img src="img/paw.png" name="paw" width="15" height="15" /> Puppies</a></li>
+	<li id="Info"><a href="info.html"><img src="img/paw.png" name="paw" width="15" height="15"/> Info</a> </li>
+</div>
 
   <div class="logo">
     <a href="index.php"> <img src="img/logo.png" alt="Logo" name="logo" width="200" height="200"/></a>
