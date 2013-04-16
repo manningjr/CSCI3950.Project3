f46af17d3546a8a6fd043ce11a589004c48c5851
Rob Manning <Jman_1106@gcsu202044.gcsu.edu>
2013-04-16 15:11:18 -0400

    Made modifications

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <?php
 ini_set('display_errors', 1);
 
-echo"<div class='login'>";
+/*
 echo"<p><h3> Login</h3>";
                     echo"<table border='0'>";
                         echo"<form method='POST' action='loginproc.php'";
@@ -34,6 +34,56 @@ echo"<p><h3> Register</h3>";
                         echo"<td><input type='submit'value='Submit'></td></tr>";
                         echo"</form>";
                     echo"</table>";
-                echo"</div>";
+                echo"</div>"; */
  ?>
+ 
+ <div class="login">
+ 	<h3>Login</h3>
+    <table border="0">
+    	<form method="post" action="loginproc.php">
+        	
+            <tr>
+            	<td>Username: </td>
+                <td><input type="text" name="username" size="20"/></td>
+            </tr>
+            
+            <tr>
+            	<td>Password: </td>
+                <td><input type="text" name="password" size="20"/></td>
+            </tr>
+            
+            <tr>
+            	<td><a href='#'><u>forgot password</u>&nbsp;</a></td>
+            </tr>
+            
+            </table
+            	
+        </form>
+    </table>
+    
+    <h3>Register</h3>
+    <table border="0">
+    	<form method="post" action="loginproc.php">
+        	
+            <tr>
+            	<td>Username: </td>
+                <td><input type="text" name="username" size="20"/></td>
+            </tr>
+            
+            <tr>
+            	<td>Password: </td>
+                <td><input type="text" name="password" size="20"/></td>
+            </tr>
+            
+            <tr>
+            	<td>Verify Password: </td>
+                <td><input type="text" name="verifypassword" size="20"/></td>
+            </tr>
+            
+            </table
+            	
+        </form>
+    </table>
+ </div>
+ 
 </div>
