4bc0830f45e7df79b82f43e30f32f7044bafdb8f
Harrison Statham <harrison.statham@comcast.net>
2013-04-19 13:10:01 -0300

    Update pupCart.php

--- pupCart.php
+++ pupCart.php
@@ -9,7 +9,14 @@
 	$get = $_GET;
 
 	$id = $get['id'];
-
+	
+	
+	// nickorus you need to do something like this:
+	
+	$session['cart'][] = $id;
+	
+	// not if your looking to push the $id on to the array.
+	
 	$session['cart'] = $id;
 
 	print_r($session);
