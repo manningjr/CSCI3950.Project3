f93f7b9c3e40cffb09eb126135a0a2643c5784f1
Michael Tan <michaeltan@Michaels-MacBook-Pro.local>
2013-04-19 03:27:31 -0400

    Added dogs to the database

--- PupFetcher.sql
+++ PupFetcher.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 16, 2013 at 06:37 AM
+-- Generation Time: Apr 19, 2013 at 09:27 AM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -57,7 +57,25 @@ CREATE TABLE `puppies` (
 
 INSERT INTO `puppies` (`puppy_id`, `name`, `breed`, `sex`, `birthday`, `location`, `price`, `vaccination`, `img_name`) VALUES
 (1, 'Turk', 'Lab', 'Male', '03-02-2013\n', 'Oregon', '\n999', 1, 'img/puppic/\n11.jpg'),
-(2, 'Buddy', 'Golden Retriever', 'Male', '04-10-2013', 'Florida', '\n1099', 1, 'img/puppic/\n2.jpg');
+(2, 'Buddy', 'Golden Retriever', 'Male', '04-10-2013', 'Florida', '\n1099', 1, 'img/puppic/\n2.jpg'),
+(13, 'Cinnamon', 'Irish Setter', 'Male', '04-10-2013', 'Missouri', '800', 1, 'img/puppic/13.jpg'),
+(14, 'Charles', 'Irish Wolfhound', 'Male', '12-02-2011', 'Georgia', '600', 1, 'img/puppic/14.jpg'),
+(15, 'Droopy', 'Bloodhound', 'Male', '01-13-2013', 'Mississippi', '700', 2, 'img/puppic/15.jpg'),
+(16, 'Lady', 'Cocker Spaniel', 'Female', '03-20-2013', 'Kentucky', '725', 1, 'img/puppic/16.jpg'),
+(17, 'Benji', 'Icelandic Sheepdog', 'Male', '04-01-2013', 'Colorado', '755', 2, 'img/puppic/17.jpg'),
+(18, 'Lucca', 'Irish Doodle', 'Male', '06-10-2012', 'Ohio', '600', 1, 'img/puppic/18.jpg'),
+(19, 'Peach', 'Sheepdog', 'Female', '01-24-2013', 'Montana', '700', 1, 'img/puppic/19.jpg'),
+(20, 'Bella', 'Shih Tzu', 'Female', '03-01-2013', 'Missouri', '775', 1, 'img/puppic/20.jpg'),
+(21, 'Bernie', 'Saint Bernard', 'Female', '02-04-2013', 'Montana', '700', 1, 'img/puppic/21.jpg'),
+(22, 'Samster', 'German Sheperd', 'Male', '04-02-2013', 'Texas', '800', 2, 'img/puppic/22.jpg'),
+(23, 'Bailey', 'Bernese Mountain', 'Male', '03-23-2013', 'Washington', '800', 1, 'img/puppic/23.jpg'),
+(24, 'Kola', 'Labrador', 'Male', '04-03-2013', 'Georgia', '600', 2, 'img/puppic/24.jpg'),
+(25, 'Trip', 'Border Collie', 'Male', '02-17-2013', 'Rhode Island', '715', 1, 'img/puppic/25.jpg'),
+(26, 'Nola', 'Cocker Spaniel', 'Female', '04-08-2013', 'Virginia', '700', 2, 'img/puppic/26.jpg'),
+(27, 'Aussie', 'Australian Cattle', 'Male', '04-06-2013', 'North Carolina', '680', 2, 'img/puppic/27.jpg'),
+(28, 'Traveler', 'Beagle', 'Male', '03-11-2013', 'Colorado', '700', 1, 'img/puppic/28.jpg'),
+(29, 'Pooh', 'Chow Chow', 'Female', '04-01-2013', 'Maryland', '800', 1, 'img/puppic/29.jpg'),
+(30, 'Wishbone', 'Jack Russel', 'Male', '02-05-2013', 'Georgia', '650', 1, 'img/puppic/30.jpg');
 
 -- --------------------------------------------------------
 
