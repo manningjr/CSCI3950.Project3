fad9a161e01e9c5d53e420745b111985cc9e4336
Nick Morelli <morelli3135@Nick-Morellis-MacBook-Pro.local>
2013-04-27 18:01:29 -0400

    Fixed other link

--- search.php
+++ search.php
@@ -236,7 +236,7 @@
 				echo "</div>";
 		
 				echo "<div class='infoButtons'>";
-					echo"<a href='addToCart.php?id=" . $puppy_id . "'> <img src='img/pupcart2.png'/></a>";
+					echo"<a href='pupCart.php?id=" . $puppy_id . "'> <img src='img/pupcart2.png'/></a>";
 				echo "</div>";
 
 			}
