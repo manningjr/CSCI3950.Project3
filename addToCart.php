f87d6b1ece2cfbec1d7885b68ae1459048140761
Nick Morelli <morelli3135@Nick-Morellis-MacBook-Pro.local>
2013-04-27 19:48:40 -0400

    Fixed an error that occurred when you reloaded the cart after adding a puppy that's already in it

--- addToCart.php
+++ addToCart.php
@@ -1,35 +1,38 @@
 	<?php
 
-		$get = $_GET;
-		$id = $get['id'];
-
-		//	Test to see if item is already in cart. If it is, don't add it and give an error message. If it is not, add it into the session
-		$temp;
-		foreach ($_SESSION['cart'] as $s)
+		if (isset($_GET) && $_GET != null)
 		{
-			if ($s == $id)
+			$get = $_GET;
+			$id = $get['id'];
+
+			//	Test to see if item is already in cart. If it is, don't add it and give an error message. If it is not, add it into the session
+			$temp;
+			foreach ($_SESSION['cart'] as $s)
 			{
-				$temp = true;
-			}
-		}
-		
-		if ($temp != true)
-		{
-			$_SESSION['cart'][] = $id;
-		}
-		else
-		{
-			?>
-
-			<script>
-				onload = function f()
+				if ($s == $id)
 				{
-					alert("Puppy already in cart");
+					$temp = true;
 				}
-			</script>
+			}
+			
+			if ($temp != true)
+			{
+				$_SESSION['cart'][] = $id;
+			}
+			else
+			{
+				?>
 
-			<?php
+				<script>
+					onload = function f()
+					{
+						alert("Puppy already in cart");
+					}
+				</script>
 
+				<?php
+
+			}
 		}
 
 	?>
