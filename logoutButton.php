1f141ffe5709672c36c96597328719bc026c5131
Rob Manning <Jman_1106@gcsu202044.gcsu.edu>
2013-04-27 16:48:10 -0400

    Logout Button Length Fixed

--- logoutButton.php
+++ logoutButton.php
@@ -2,13 +2,13 @@
  <a href="pupCart.php"><p><img src="img/cart.png"</p></a>
 </div>
  
- <span class="toggle2">
+ <span class="toggle3">
 </span>
 
 <div class="separation-letters">
  <p>|</p>
 </div>
 
- <div class="toggle2-letters">
+ <div class="toggle3-letters">
 <a href="logout.php"><p><b>Logout</b></p></a>
 </div>
