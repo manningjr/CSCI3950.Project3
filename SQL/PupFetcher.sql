b0471c9cc38edfd5029e03f31f5da7af4d4e98f8
Rob Manning <Jman_1106@gcsu202044.gcsu.edu>
2013-04-30 14:05:37 -0400

    new db

--- SQL/PupFetcher.sql
+++ SQL/PupFetcher.sql
@@ -3,16 +3,24 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 19, 2013 at 10:56 PM
+-- Generation Time: Apr 30, 2013 at 08:02 PM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `PupFetcher`
 --
+CREATE DATABASE `PupFetcher` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `PupFetcher`;
 
 -- --------------------------------------------------------
 
@@ -82,15 +90,28 @@ CREATE TABLE `user` (
   `member_id` int(4) NOT NULL AUTO_INCREMENT,
   `username` varchar(65) NOT NULL,
   `password` varchar(65) NOT NULL,
-  `name` varchar(50) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `company` varchar(15) NOT NULL,
+  `phoneNum` int(15) NOT NULL,
   `card_number` int(16) NOT NULL,
   `security_code` int(3) NOT NULL,
   `street` varchar(150) NOT NULL,
-  `apt` int(10) NOT NULL,
   `city` varchar(50) NOT NULL,
   `state` varchar(15) NOT NULL,
   `zip` varchar(5) NOT NULL,
   PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`member_id`, `username`, `password`, `fname`, `lname`, `email`, `phoneNum`, `card_number`, `security_code`, `street`, `city`, `state`, `zip`) VALUES
+(1, 'user', 'useruser', 'John', 'Doe', 'johndoe@gcsu.com', 0, 2147483647, 555, '441 street', 'milledgeville', 'ga', '31061'),
+(2, 'test', 'testing1', 'Test', 'User', 'test@gcsu.com', 0, 2147483647, 321, 'Testing Dr.', 'testingville', 'TV', '54321'),
+(4, 'MDoe', 'password', 'Mary', 'Doe', 'mary.doe@gcsu.com', 2147483647, 0, 0, '42 Lucky St.', 'Puppies', 'CA', '85921');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
